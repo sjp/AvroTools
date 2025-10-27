a3da99978fe09c10b4bf799cabbae68b58a24fa4
Simon Potter <simon@sjp.co.nz>
2025-10-27 21:23:43 +1300

    Tidy up usings

--- src/SJP.Avro.Tools/Idl/IdlTokenizer.cs
+++ src/SJP.Avro.Tools/Idl/IdlTokenizer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Superpower;
 using Superpower.Model;
 
--- src/SJP.Avro.Tools/Schema/Model/EnumType.cs
+++ src/SJP.Avro.Tools/Schema/Model/EnumType.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace SJP.Avro.Tools.Schema.Model;
