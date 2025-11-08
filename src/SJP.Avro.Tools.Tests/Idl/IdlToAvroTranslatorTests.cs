c7769f500c182d41d7ed0457a6800d463efcadad
Simon Potter <simon@sjp.co.nz>
2025-11-08 14:41:56 +1300

    Test cleanup

--- src/SJP.Avro.Tools.Tests/Idl/IdlToAvroTranslatorTests.cs
+++ src/SJP.Avro.Tools.Tests/Idl/IdlToAvroTranslatorTests.cs
@@ -1,19 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.Extensions.FileProviders;
 using NUnit.Framework;
 using SJP.Avro.Tools.Idl;
-using SJP.Avro.Tools.Idl.Model;
 
 namespace SJP.Avro.Tools.Tests.Idl;
 
 [TestFixture]
-internal class IdlCompilerTests
+internal static class IdlToAvroTranslatorTests
 {
     private const string BaseInputNamespace = "SJP.Avro.Tools.Tests.Idl.Data.Input";
     private const string BaseOutputNamespace = "SJP.Avro.Tools.Tests.Idl.Data.Output";
@@ -21,72 +19,17 @@ internal class IdlCompilerTests
     private static readonly IFileProvider InputFileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), BaseInputNamespace);
     private static readonly IFileProvider OutputFileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly(), BaseOutputNamespace);
 
-    private IdlCompiler _compiler;
-
-    [SetUp]
-    public void Setup()
-    {
-        var fileProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
-        _compiler = new IdlCompiler(fileProvider);
-    }
-
-    [Test]
-    public static void Ctor_GivenNullFileProvider_ThrowsArgNullException()
-    {
-        Assert.That(() => new IdlCompiler(null), Throws.ArgumentNullException);
-    }
-
-    [Test]
-    public void Compile_GivenNullFilePath_ThrowsArgNullException()
-    {
-        var testProtocol = new Protocol(
-            null,
-            new Identifier("fake_protocol"),
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            new Dictionary<Identifier, MessageDeclaration>()
-        );
-
-        Assert.That(() => _compiler.Compile(null, testProtocol), Throws.ArgumentNullException);
-    }
-
-    [TestCase("")]
-    [TestCase("        ")]
-    public void Compile_GivenEmptyOrWhiteSpaceFilePath_ThrowsArgException(string filePath)
-    {
-        var testProtocol = new Protocol(
-            null,
-            new Identifier("fake_protocol"),
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            new Dictionary<Identifier, MessageDeclaration>()
-        );
-
-        Assert.That(() => _compiler.Compile(filePath, testProtocol), Throws.ArgumentException);
-    }
-
-    [Test]
-    public void Compile_GivenNullProtocol_ThrowsArgNullException()
-    {
-        Assert.That(() => _compiler.Compile("fake_path", null), Throws.ArgumentNullException);
-    }
-
     [TestCaseSource(nameof(IdlInputOutputFilenames))]
     public static async Task Tokenize_GivenValidIdlInput_MatchesExpectedOutput(string idlSampleResourceName, string avroSampleResourceOutput)
     {
         var inputFile = InputFileProvider.GetFileInfo(idlSampleResourceName);
         var outputFile = OutputFileProvider.GetFileInfo(avroSampleResourceOutput);
 
-        using var inputReader = new StreamReader(inputFile.CreateReadStream());
-        using var outputReader = new StreamReader(outputFile.CreateReadStream());
+        await using var inputFileReadStream = inputFile.CreateReadStream();
+        await using var outputFileReadStream = outputFile.CreateReadStream();
+
+        using var inputReader = new StreamReader(inputFileReadStream);
+        using var outputReader = new StreamReader(outputFileReadStream);
 
         var inputContents = await inputReader.ReadToEndAsync();
         var outputContents = await outputReader.ReadToEndAsync();
